58ddf0d9ee0e21934170b56fc426fe83724cd36a
ne0n9uy <2leebins@naver.com>
2022-10-22 23:58:33 +0900

    editted

--- HW02/HW_JS.js
+++ HW02/HW_JS.js
@@ -1,69 +1,60 @@
 import * as THREE from 'three';
-import {OrbitControls} from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+// import {OrbitControls} from "../node_modules/three/examples/jsm/controls/OrbitControls.js";
+import {AxesHelper} from '../node_modules/three/src/helpers/AxesHelper.js'
 
 var myRenderer;
-var myScene;
 var myCamera;
+var myScene;
 
-//renderer setting    
+//*renderer setting    
 myRenderer=new THREE.WebGL1Renderer();
 const rd_w=640; //window.innerWidth;
 const rd_h=480; //window.innerHeight;
 myRenderer.setSize(rd_w,rd_h);
-myRenderer.setSize(rd_w,rd_h);
 myRenderer.setViewport(0,0,rd_w,rd_h);
-
 const container=document.getElementById('myContainer');
 container.appendChild(myRenderer.domElement);
-//camera setting
+//*camera setting
 myCamera=new THREE.PerspectiveCamera(45,rd_w/rd_h,1,500);
 myCamera.position.set(0,0,50);
 myCamera.up.set(0,1,0);
 myCamera.lookAt(0,0,0);
-//scene setting
+//*scene setting
 myScene=new THREE.Scene();
+//*보조축 추가
+var axes=new THREE.AxesHelper(20); 
+myScene.add(axes);  
 
-//거리 체크
-// myScene.updateMatrixWorld(true);
-// cps.setFromMatrixPosition(camera.matrixWorld);
-// ops.setFromMatrixPosition(myMesh.matrixWorld);
-// console.log(cps.z-ops.z);
-
-const controls=new OrbitControls(myCamera,myRenderer.domElement);
+// const controls=new OrbitControls(myCamera,myRenderer.domElement);
 
 //geometry setting: Make a Cube!
 const points=[ //cubeSize==10
-    5,5,5,  //1
-    5,5,-5,  //0
-    5,-5,5, //4
-    5,-5,-5, //5
-    -5,5,-5,//2    
-    -5,5,5,  //3
+    5,5,5,  //0
+    5,5,-5,  //1
+    5,-5,5, //2
+    5,-5,-5, //3
+    -5,5,-5,//4  
+    -5,5,5,  //5
     -5,-5,-5, //6
-    -5,-5,5 //7
-
-    // 0,0,0,  10,0,0, 10,10,0,    0,10,0,
-    // 0,0,10, 10,0,10,    10,10,10,   0,10,10,
-    
+    -5,-5,5 //7   
 ];
-const indicies=[
-    // 2,1,3,  6,1,2,  6,5,1,  6,4,5,
-    // 3,5,0,  3,1,5,  7,6,2,  7,4,6,
-    // 7,3,0,  7,2,3,  5,7,0,  5,4,7
 
-    //대충 맞음
-    // 1,3,6,  3,2,6,  3,1,2,  //6,1,2, 
-    //  1,6,4,  5,1,4,  5,4,6,  //1,5,6,
-    // 6,2,7,  7,2,3,  7,5,6, // 5,2,6,
-    // 5,2,0,  5,0,1,  2,1,0,  //1,2,5,      
-    0,2,1, 2,3,1, //front
-    4,5,1, 5,0,1,  //right
-    7,6,2,  6,3,2,  //left
-    4,6,5, 6,7,5,  //back
-    5,7,0,  7,2,0,  //top
-    1,3,4,  3,6,4,  //bottom     
+const indicies=[  
+    5,6,7, 4,6,5, //front
+    2,0,5, 2,5,7, //right
+    6,4,1, 6,1,3, //left
+    3,1,2, 2,1,0, //back
+    5,0,1, 5,1,4, //top
+    2,7,6, 2,6,3, //bottom
 ]
 
+    // 0,2,1, 2,3,1, //front
+    // 4,5,1, 5,0,1,  //right
+    // 7,6,2,  6,3,2,  //left
+    // 4,6,5, 6,7,5,  //back
+    // 5,7,0,  7,2,0,  //top
+    // 1,3,4,  3,6,4,  //bottom     
+    
 const cubeGeo=new THREE.BufferGeometry();
 const pointsArray=new Float32Array(points);
 
@@ -74,34 +65,32 @@ const cubeMaterial=new THREE.MeshBasicMaterial( { color: '#38E54D', wireframe:tr
 const myMesh = new THREE.Mesh( cubeGeo, cubeMaterial );
 
 myScene.add(myMesh);
-controls.update();
+// controls.update();
 myRenderer.render(myScene,myCamera);
 //여기까지 object 생성 및 scene에 render
 
+//*camera -object 간 거리 체크 => 15
+console.log(myCamera.position.distanceTo(myMesh.position));
 
 //* Event
 animate();
 //*create animation
 function animate(){
 requestAnimationFrame(animate);
-controls.update();
+// controls.update();
 myRenderer.render(myScene,myCamera);
 }
 
-myRenderer.domElement.style="touch-action:none";
-
-myRenderer.domElement.onpointerdown=mouseDownHandler;
+//*register events to keyboard and mouse
+document.addEventListener('keydown',keyDownHandler);
+// myRenderer.domElement.onkeyup=keyUpHandler;
 // myRenderer.domElement.oncontextmenu=RbtnHandler;
-// myRenderer.domElement.onpointermove=mouseMoveHandler;
-// myRenderer.domElement.onpointerup=mouseUpHandler;
-// myRenderer.domElement.onpointercancel=mouseUpHandler;
-// myRenderer.domElement.onpointerout=mouseUpHandler;
-// myRenderer.domElement.onpointerleave=mouseUpHandler;
 
-//Compute SS -> WS position
-function compute_pos_ss2ws(x_ss,y_ss){
-    return new THREE.Vector3(x_ss/rd_w*2-1, -y_ss/rd_h*2+1,-1).unproject(myCamera);
-}
+myRenderer.domElement.onpointermove=MouseMoveHandler;
+myRenderer.domElement.onpointerup=mouseUpHandler;
+myRenderer.domElement.onpointercancel=mouseUpHandler;
+myRenderer.domElement.onpointerout=mouseUpHandler;
+myRenderer.domElement.onpointerleave=mouseUpHandler;
 
 //*Check MouseBtn
 // document.addEventListener('mouseup', function(e) {
@@ -113,40 +102,127 @@ function compute_pos_ss2ws(x_ss,y_ss){
 //         case 2: console.log('right'); break;  //btnCode == 2
 //     }
 // });
-let mouse_btn_flag=false;
-function mouseDownHandler(e){
-    // var e = e || window.event;
-    // var btnCode = e.button;
+
+function keyDownHandler(e){
+    console.log(e.key); 
+    if(e.key=='O' | e.key=='o'){
+        console.log('Obj Transform');
+        myRenderer.domElement.onpointerdown=obj_MouseDownHandler;
+        obj_MouseDownHandler(e);
+    }
+    else if(e.key=='C' | e.key=='c'){
+        console.log('Cmr Transform');
+        myRenderer.domElement.onpointerdown=cmr_MouseDownHandler;
+        cmr_MouseDownHandler(e);
+    }
+}
+
+//*Object Transform Function
+let mouse_btn_flag=false;  //mouse down 여부
+function obj_MouseDownHandler(e){
+    if(e.pointerType =='mouse'){
+        mouse_btn_flag=true;     
+   
+        // myRenderer.domElement.onpointerdown=obj_MouseDownHandler;
+        if(e.button==0){
+        //LBtn
+//*MouseEvent(1-1)  Move Object: Rotate by Lbtn mouse drag
+            console.log('objM_Mouse Left Btn Down');
+       
+            MouseMoveHandler(e);
+            
+        }
+        else if(e.button==2){
+        //RBtn
+        //MouseEvent(1-2)  Move Object: Move the cube parallel to the image plane by Rbtn mouse drag
+            console.log('objM_Mouse Right Btn Down');
+            
+            
+            
+            
+            MouseMoveHandler(e);
+
+        }
+    }
+}
+
+let x_prev=rd_w/2;
+let y_prev=rd_h/2;
+//Compute SS -> WS position
+function compute_pos_ss2ws(x_ss,y_ss){
+    return new THREE.Vector3(x_ss/rd_w*2-1, -y_ss/rd_h*2+1,-1).unproject(myCamera);
+}
+
+function MouseMoveHandler(e){
+    //마우스 움직일 시 함수
+    //L, R 버튼 if문으로 구분해서 추가하기
+    
+    if(e.pointerType=='mouse'){
+            mouse_btn_flag=true;
+      
+            const myPosPS=new THREE.Vector3(
+                e.clientX/rd_w * 2 - 1,  //0~ 1 -> 0~ 2 , -1 ==1  => x를 -1~ 1로 매핑
+                -e.clientY/rd_h*2+1, //screen(아래쪽) 과 proj y방향 반대
+                -1);
+            const myPosWS=myPosPS.clone();
+            myPosWS.unproject(myCamera);
+            
+            // console.log("Mouse Pos PS:",myPosPS.x, myPosPS.y, myPosPS.z);
+            // console.log("Mouse Pos WS:",myPosWS.x, myPosWS.y, myPosWS.z);
+
+            if(mouse_btn_flag){ //마우스 다운이면
+                let posNp=compute_pos_ss2ws(e.clientX, e.clientY);
+               
+                x_prev=e.clientX;
+                y_prev=e.clientY;
+             
+
+                // myMesh.setRotationFromQuaternion(new THREE.Quaternion().setFromAxisAngle(new THREE.Vector3(0, 1, 0), Math.PI / 4));
+            
+                // let matLocal=new THREE.Matrix4();
+                // const matR=new THREE.Matrix4().makeRotationAxis(new THREE.Vector3(0,1,0),Math.PI/4); 
+                // matLocal.premultiply(matR);
+                // myMesh.matrix=matLocal.clone();
+                // myMesh.matrixAutoUpdate=false;
+                // console.log(myMesh.matrix.elements);
+
+
+              
+            }
+              
+            
+         }
+    }
+function mouseUpHandler(e){
+    mouse_btn_flag=false;
+}
+//*Camera Transform Function
+function cmr_MouseDownHandler(e){
     if(e.pointerType =='mouse'){
         mouse_btn_flag=true;
-        //LBtn
+        // myRenderer.domElement.onpointerdown=cmr_MouseDownHandler;
+//카메라도 마우스 무브 추가하기
         if(e.button==0){
-            console.log('Mouse Left Btn Down');
+        //LBtn
+            console.log('cmrM_Mouse Left Btn Down');
         }
         else if(e.button==1 ){
-            //Wheel
-            console.log('Mouse Middle Btn(Wheel) Down');
+        //Wheel
+            console.log('cmrM_Mouse Middle Btn(Wheel) Down');
 
         }
         else if(e.button==2){
-            //RBtn
-            console.log('Mouse Right Btn Down');
+        //RBtn
+            console.log('cmrM_Mouse Right Btn Down');
 
         }
     }
 }
 
 
-//MouseEvent(1-1)  Move Object: Rotate by Lbtn mouse drag
-
-
-//MouseEvent(1-2)  Move Object: Move the cube parallel to the image plane by Rbtn mouse drag
-
 //MouseEvent(2-1)  Move Camera: Rotate by Lbtn mouse drag
-
 //MouseEvent(2-2)  Move Camera : Move the camera by mouse wheels
-
 //MouseEvent(2-3)  Move Camera : Move the camera parallel to the image plane by by Rbtn mouse drag
 
 
-//나중에 리사이즈 이벤트 추가하기
+//나중에 rd 사이즈 inner로 바꾸고 리사이즈 이벤트 추가하기
