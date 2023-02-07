3d4f18d86015a8ca83730e1051c837402f797ae7
ne0n9uy <2leebins@naver.com>
2023-02-07 20:31:48 +0900

    objectSize

--- universe/active.js
+++ universe/active.js
@@ -1,9 +1,8 @@
 import * as THREE from 'three';
+import { MaterialLoader, Mesh } from 'three';
 import {OrbitControls} from '../node_modules/three/examples/jsm/controls/OrbitControls.js';
 import { OBJLoader } from '../node_modules/three/examples/jsm/loaders/OBJLoader.js';
 import { MTLLoader } from '../node_modules/three/examples/jsm/loaders/MTLLoader.js';
-import { Object3D } from 'three';
-import { BoxHelper } from '../node_modules/three/src/helpers/BoxHelper.js';
 
 
 /* Basic Setting*/
@@ -41,21 +40,20 @@ myScene.background = new THREE.Color('#150050');
 
 
 
-// /*Controller / Loader*/
+/*Controller / Loader*/
 const ctrl=new OrbitControls(myCamera,myRenderer.domElement);
-// ctrl.update();
+ctrl.update();
 
 
 /*Mtl Load */
 const mtlLoader=new MTLLoader();
 
-mtlLoader.load('./obj/earth.mtl', function (materials){
+mtlLoader.load('./obj/sun.mtl', function (materials){
     materials.preload();
     objLoader(materials);
 });
 //why work?    
 
-var earth, sun, ufo, et, light1, light2, light2, light3, moon, saturn, train999, rckt;
 
 /*Obj Load */
  function objLoader(materials){
@@ -63,129 +61,98 @@ var earth, sun, ufo, et, light1, light2, light2, light3, moon, saturn, train999,
      objLoader.setMaterials(materials);
      // objLoader.setPath('./obj/');
 
-     objLoader.load('./obj/earth.obj',function (obj){
-         earth=obj;
+     var earth= objLoader.load('./obj/earth.obj',function (earth){
+         console.log(earth.position);
          earth.position.set(2,-2.5,0);
          earth.rotation.y+=0.5;
          myScene.add(earth);
-         console.log("earth: " ,earth.position);
      });
-    objLoader.load('./obj/sun.obj',function (obj){
-        sun=obj;
+
+    var sun = objLoader.load('./obj/sun.obj',function (sun){
+        console.log(sun.position);
+        let box=new THREE.Box3().setFromObject(sun);
+        // let sunSize=box.max.x- box.min.y;
+        // console.log("sunSize", sunSize);
+        // console.log("sun", sun.position);
         sun.position.set(0,-0.5,0);
-        sun.scale.x=1.5;
-        sun.scale.y=1.5;
         myScene.add(sun);
-        console.log("sun: ",sun.position);
     });
-    objLoader.load('./obj/ufo.obj',function (obj){
-        ufo=obj;
+    var ufo = objLoader.load('./obj/ufo.obj',function (ufo){
         ufo.position.set(-4.5,1.3,0);
         ufo.rotation.z+=0.2;
         ufo.rotation.x+=0.3;
-        ufo.scale.x=1.5;
-        ufo.scale.y=1.5;
         myScene.add(ufo);
-        console.log("ufo: ",ufo.position);
     });
-    objLoader.load('./obj/et.obj',function (obj){
-        et=obj;
+    var et = objLoader.load('./obj/et.obj',function (et){
+        //console.log(et.position);
         et.position.set(-3.5,0,0);
         myScene.add(et);
-        console.log("et: ",et.position);
     });
+    
     //big light
-    objLoader.load('./obj/light1.obj',function (obj){
-        light1=obj;
+    var light1 = objLoader.load('./obj/light1.obj',function (light1){
+        console.log(light1.position);
         light1.position.set(4.3,3.3,0);
         myScene.add(light1);
-        console.log("light1: ",light1.position);
     });
+
     //middle light
-    objLoader.load('./obj/light2.obj',function (obj){
-        light2=obj;
+    var light2 = objLoader.load('./obj/light2.obj',function (light2){
+        console.log(light2.position);
         light2.position.set(3.3,2.3,0);
         myScene.add(light2);
-        console.log("light2: ",light2.position);
     });
+
     //small light
-    objLoader.load('./obj/light3.obj',function (obj){
-        light3=obj;
+    var light3 = objLoader.load('./obj/light3.obj',function (light3){
+        console.log(light3.position);
         light3.position.set(2.5,1.5,0);
         myScene.add(light3); 
-        console.log("light3: ",light3.position);
     });
-    objLoader.load('./obj/moon.obj',function (obj){
-        moon=obj;
+    var moon = objLoader.load('./obj/moon.obj',function (moon){
+        console.log(moon.position);
         moon.position.set(-1.8,2.3,0);
         moon.rotation.x+=0.1;
-        moon.scale.x=1.5;
-        moon.scale.y=1.5;
+
         myScene.add(moon);
-        console.log("moon: ",moon.position);
     });
-    objLoader.load('./obj/saturn.obj',function (obj){
-        saturn=obj;
-        saturn.position.set(0,3.6,0);
+    var saturn = objLoader.load('./obj/saturn.obj',function (saturn){
+        console.log(saturn.position);
+        saturn.position.set(0,3.8,0);
         saturn.rotation.z-=0.2;
         saturn.rotation.x+=0.5;
-        saturn.scale.x*=2;
-        saturn.scale.y*=2;
         myScene.add(saturn);
-        console.log("saturn: ",saturn.position);
     });
-    objLoader.load('./obj/rckt.obj',function (obj){
-        rckt=obj;
+    var rckt = objLoader.load('./obj/rckt.obj',function (rckt){
+        console.log(rckt.position);
         rckt.position.set(4,-4,0);
         rckt.rotation.z+=0.5;
         rckt.rotation.y+=0.3;
         myScene.add(rckt);
-        console.log("rckt: ",rckt.position);
     });
-    objLoader.load('./obj/train999.obj',function (obj){
-        train999=obj;
+    var train999 = objLoader.load('./obj/train999.obj',function (train999){
+        console.log(train999.position);
         train999.position.set(-3,-3.5,0);
         train999.rotation.z+=0.7;
         train999.rotation.x+=0.5;
-        train999.name="train999";
         myScene.add(train999);
-        console.log("train: ",train999.position);
-        train999.addEventListener('mousedown',R());
     });
+
 }
 
-console.log("Components: " , myScene.children);  //group 누군지 아는 법
 
 //* Events
-function R(obj){
-    // obj.rotation.x+=0.8;
-    console.log(3);
-}
-// var t=myScene.getObjectByName("train999");
-// console.log("t: ",t);
-// console.log(1, rckt);  //undefined
-    
-
-
-
-var cmrPst=myCamera.position.z;
+// train999=
+// train999.addEventListener("click",R);
+// function R(){
+//     Mesh.rotation.x+=0.5;
+// }
 //*create animation
 function animate(){
     ctrl.update();
+    
     requestAnimationFrame(animate);
     myRenderer.render(myScene,myCamera);
-    // document.addEventListener("keyup",function GO(e){
-        
-    //     if(e.key== " " ||e.code=="Space")
-    //     {  
-    //         myCamera.position.z+=1.5;
-    //         console.log(cmrPst);
-    //         console.log(myCamera.position.z);
-
-    //         if(myCamera.position.z>15){
-    //         myCamera.position.z+=0; }
-    //     }        
-    // });
 }
 animate();
 
